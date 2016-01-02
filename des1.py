89b67de2bf3baaf7bf61bb9f8b07bd9c2c10cd99
fish7dining <474245125@qq.com>
2016-01-03 07:43:13 +0800

    decryption not success

--- des1.py
+++ des1.py
@@ -10,6 +10,7 @@ class des:
                        63,55,47,39,31,23,15,7,62,54,46,38,30,22,
                        14,6,61,53,45,37,29,21,13,5,28,20,12,4]
         self.table3 = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
+        self.table3_2 = [0,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
         self.table4 = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,
                         26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,
                         51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
@@ -109,7 +110,7 @@ class des:
         d = temp1[28:]
         self._16_KEY48_2 = []
         for i in range(16):
-            w1 = self.table3[i]
+            w1 = self.table3_2[i]
             cc = c[28 - w1:]
             for j in range(0, 28 - w1):
                 cc.append(c[j])
@@ -200,7 +201,7 @@ class des:
         self.generate_key()
         self.generate_16_key()
         self.mainProcess()
-        self.Output()
+        #self.Output()
     def decryption(self, ):
         for i in self.DATA64:
             print i
